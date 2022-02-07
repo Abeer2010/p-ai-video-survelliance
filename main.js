3b21b88801444906a7622d35defff74231f87101
Abeer2010 <78603161+Abeer2010@users.noreply.github.com>
2022-02-07 20:50:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+video = "";
+function setup()
+{
+    canvas = createCanvas("480,380");
+    canvas.center();
+    video = createVideo("video.mp4");
+    video.hide();
+}
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    document.getElementById("input").value;
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+}
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
